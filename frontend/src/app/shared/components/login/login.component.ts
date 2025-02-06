af5b61739b05db42bb62198d29fb6c996e8d1870
Ravi Kumar <rvcoder6684@gmail.com>
2025-02-07 00:57:26 +0530

    Added folders and login files

---
+++ frontend/src/app/shared/components/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb : FormBuilder, private authService : AuthService, private router : Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+   }
+
+  ngonInit(): void {}
+
+  onSubmit() {
+    console.log("hii");
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe({
+        next: (res) => {
+          localStorage.setItem('token', res.token);
+          alert('Login successful');
+        },
+        error: (err) => {
+          console.error('Login failed:', err);
+          alert('Invalid credentials');
+        },
+      });
+    }
+  }
+
+}
