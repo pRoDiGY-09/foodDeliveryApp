67eeaafee099bc9c0f275c9a00adffc2b1a5e2dc
Ravi Kumar <rvcoder6684@gmail.com>
2025-02-07 22:22:37 +0530

    updated auth.service & app.routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './shared/components/login/login.component';
+import { SignupComponent } from './shared/components/signup/signup.component';
+import { HomeComponent } from './shared/components/home/home.component';
 
 export const routes: Routes = [
+    {path:'home', component:HomeComponent},
     {path:'login', component: LoginComponent},
-    {path:'', redirectTo: 'login', pathMatch: 'full'},
-    {path:'**', redirectTo: 'login', pathMatch: 'full'}
+    {path:'signup', component: SignupComponent},
+    {path:'', redirectTo: 'home', pathMatch: 'full'},
+    {path:'**', redirectTo: 'home', pathMatch: 'full'}
 ];
